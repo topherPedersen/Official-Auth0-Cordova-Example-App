8603b370a8ddc5a7c22e1d9c662c01a864a545da
Christopher Pedersen <12384786+topherPedersen@users.noreply.github.com>
2019-11-08 21:24:31 -0600

    Add files via upload

---
+++ OfficialCordovaAuth0Example/env.js
@@ -0,0 +1,6 @@
+module.exports = {
+  AUTH0_DOMAIN: 'topherpedersen.auth0.com',
+  AUTH0_CLIENT_ID: 'GDXUhFQMxxY7A8mOxlWEcK9zmIoY51D1',
+  AUTH0_AUDIENCE: 'https://topherpedersen.auth0.com/userinfo',
+  PACKAGE_ID: 'com.auth0.cordova.example'
+};
